45a1ce13482850157b8f7f0fbb43d6849c7ffc6d
tegarfaris <tegarfarisn@gmail.com>
2024-02-19 09:51:24 +0700

    feat: integrate live site and github

--- src/components/page-modules/home/projects-section/index.tsx
+++ src/components/page-modules/home/projects-section/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { Badge, Button, Flex, Image, Text, Tooltip } from "@chakra-ui/react";
 import HeadingTittle from "../../../HeadingTittle";
 import { PROJECTS_ASSETS } from "../../../../helper/image.cld";
 import { IoOpenOutline } from "react-icons/io5";
@@ -15,6 +15,8 @@ const ProjectsSection: React.FC = () => {
       id: 0,
       title: "Landing Page for WO",
       image: PROJECTS_ASSETS.WO,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/landing-page",
       description:
         "The Landing Page Project for this Wedding Organizer is my first paid project, utilizing only HTML, CSS, Javascript, and Bootstrap. It was created to provide information about the upcoming event. This website also serves as a presentation platform for the client of this wedding organizer.",
       tech: ["HTML", "CSS", "Javascript"],
@@ -23,6 +25,8 @@ const ProjectsSection: React.FC = () => {
       id: 1,
       title: "Survey Web",
       image: PROJECTS_ASSETS.NOT_FOUND,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/TCI-fe",
       description:
         "This project actually serves as my learning journey in using Vue.js technology. Within this project, I implemented the usage of local storage for the first time to store the required data persistently.",
       tech: ["Vue 3", "Tailwind CSS"],
@@ -31,6 +35,8 @@ const ProjectsSection: React.FC = () => {
       id: 2,
       title: "Tokopedia Clone (Product List)",
       image: PROJECTS_ASSETS.NOT_FOUND,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/talentpoolarutala-frontend",
       description:
         "This project was created to challenge myself to see if I could replicate the UI of Tokopedia as closely as possible. Throughout its development, I encountered numerous learning opportunities, particularly in the aspects of layouting and responsive web design.",
       tech: ["React JS", "Tailwind CSS"],
@@ -39,6 +45,8 @@ const ProjectsSection: React.FC = () => {
       id: 3,
       title: "Contact List",
       image: PROJECTS_ASSETS.PHONE_BOOK,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/phone-book-project",
       description:
         "This project marks my first attempt as part of a technical test for a company. Here, I also delved into learning about integrations using GraphQL Apollo Client.",
       tech: ["React JS", "Typescript", "Chakra UI", "GraphQL", "Apollo Client"],
@@ -47,6 +55,8 @@ const ProjectsSection: React.FC = () => {
       id: 4,
       title: "Web News",
       image: PROJECTS_ASSETS.NEWS_APP,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/fe-enigma-news-apps",
       description:
         "This project is a website aimed at implementing a public API. I utilized the News API as the primary public API source for this website.",
       tech: ["Vite", "React JS", "Typescript", "Antd Design"],
@@ -55,6 +65,8 @@ const ProjectsSection: React.FC = () => {
       id: 5,
       title: "ToDo List",
       image: PROJECTS_ASSETS.TODOS,
+      liveSite: "https://to-do-apps-mu.vercel.app/",
+      github: "https://github.com/tegarfaris/toDo-apps",
       description:
         "This project is similar to typical ToDo apps and serves as a learning tool for implementing Redux Toolkit.",
       tech: ["React JS", "Typescript", "Chakra UI", "Redux Toolkit"],
@@ -63,6 +75,8 @@ const ProjectsSection: React.FC = () => {
       id: 6,
       title: "Food Apps",
       image: PROJECTS_ASSETS.FOOD_APPS,
+      liveSite: "",
+      github: "https://github.com/tegarfaris/food-apps",
       description:
         "In this project, only the UI is present and it's not integrated with a backend. The challenge lies in creating a responsive and visually appealing UI.",
       tech: ["Vite", "React JS", "Tailwind CSS"],
@@ -71,6 +85,9 @@ const ProjectsSection: React.FC = () => {
       id: 7,
       title: "CV Generator",
       image: PROJECTS_ASSETS.CV_GENERATOR,
+      liveSite:
+        "https://portfolio-generator-tegarfarisns-projects.vercel.app/portfolio",
+      github: "https://github.com/tegarfaris/portfolio-generator",
       description:
         "My latest project is a CV Generator, where users can create their CV according to their preferences and see the changes in real-time. In this project, I learned about React Hook Form.",
       tech: ["Next JS", "Typescript", "Chakra UI"],
@@ -101,7 +118,7 @@ const ProjectsSection: React.FC = () => {
             slideShadows: true,
           }}
           modules={[EffectCoverflow, Pagination]}
-          pagination={{ clickable: true }} // menambahkan pagination
+          pagination={{ clickable: true }}
         >
           <Flex className="swiper-wrapper" w="800px" justifyContent="center">
             {PROJECTS.map((project) => (
@@ -142,15 +159,38 @@ const ProjectsSection: React.FC = () => {
                       gap="5px"
                       pt="10px"
                     >
-                      <Button
-                        variant="outline"
-                        color="blue"
-                        border="1px solid blue"
-                        leftIcon={<IoOpenOutline />}
+                      <Tooltip
+                        display={project.liveSite === "" ? "flex" : "none"}
+                        label="Live not Ready"
                       >
-                        Live Site
+                        <Button
+                          as="a"
+                          variant="outline"
+                          color="blue"
+                          border="1px solid blue"
+                          leftIcon={<IoOpenOutline />}
+                          isDisabled={project.liveSite === ""}
+                          disabled
+                          target={project.liveSite !== "" ? "_blank" : "_self"}
+                          href={
+                            project.liveSite !== ""
+                              ? project.liveSite
+                              : "#projects-section"
+                          }
+                        >
+                          Live Site
+                        </Button>
+                      </Tooltip>
+                      <Button
+                        as="a"
+                        bg="linear-gradient(162deg, rgba(43,0,95,1) 0%, rgba(137,52,255,1) 100%, rgba(0,0,0,1) 100%)"
+                        leftIcon={<FaGithub />}
+                        color="white"
+                        target="_blank"
+                        href={project.github}
+                      >
+                        Github
                       </Button>
-                      <Button leftIcon={<FaGithub />}>Github</Button>
                     </Flex>
                   </Flex>
                 </Flex>
